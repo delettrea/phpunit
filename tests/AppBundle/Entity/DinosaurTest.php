3d72937b7e1be53f9a3eba32d5f1d607229ce59c
delettrea <delettrealine@gmail.com>
2018-09-30 18:49:37 +0200

    add test for test phpunit

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Tests\AppBundle\Entity;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testThatYourComputerWorks()
+
+    {
+        $this->assertTrue(true);
+    }
+
+}
